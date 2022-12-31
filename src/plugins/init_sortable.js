4679082f34942af43de6be30843ba336fb748e6c
thomas andrieu <than1544@colorado.edu>
2022-12-31 20:12:43 +0100

    stimulus

--- src/plugins/init_sortable.js
+++ src/plugins/init_sortable.js
@@ -5,3 +5,15 @@ const list = document.querySelector("#results")
 const initSortable = () => {
   Sortable.create(list)
 }
+
+export { initSortable }
+
+const initSortable = () => {
+  Sortable.create(list, {
+    ghostClass: "ghost",
+    animation: 150,
+    onEnd: (event) => {
+      alert(`${event.oldIndex} moved to ${event.newIndex}`)
+    }
+  })
+}
