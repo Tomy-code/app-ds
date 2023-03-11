a46505f8b4a76a2dd6a64ae688db67e462b33eba
thomas andrieu <than1544@colorado.edu>
2023-03-11 16:48:18 +0100

    link button to registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
     root to: "pages#home"
-    get "registration", to: "pages#registration"
+    get "registration", to: "pages#registration", as: :registration
 end
