bee0dd0471f89a070c561dd2da514cad10ef6b09
thomas andrieu <than1544@colorado.edu>
2023-07-02 13:42:50 +0200

    fixed bug of action mailer

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,6 +1,6 @@
 class NewslettersController < ApplicationController
   def send_daily
-    User.all.each do |user|
+    User.find_each do |user|
       NewsletterMailer.send_daily_newsletter(user.emailadress, "Le contenu de votre newsletter").deliver_now
     end
     redirect_to root_path, notice: 'Newsletter envoyée.'
--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,6 +1,6 @@
 class NewsletterMailer < ApplicationMailer
   def send_daily_newsletter(emailadress, content)
     @content = content
-    mail(to: emailadress, subject: 'Votre fait historique quotidien')
+    mail(to: emailadress, body: 'something', subject: 'Votre fait historique quotidien', delivery_method: :letter_opener)
   end
 end
