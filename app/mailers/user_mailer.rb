cf2ef18c4e4327097934c53ba26c9658b498810a
thomas andrieu <than1544@colorado.edu>
2023-07-11 19:43:41 +0200

    nothing changed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
-  def send_daily_newsletter(users)
-    @users = users
-    mail(to: users.emailadress, subject: 'Votre fait historique quotidien')
+  def send_daily_newsletter(user)
+    @user = user
+    mail(to: user.emailadress, subject: 'Votre fait historique quotidien')
   end
 end
