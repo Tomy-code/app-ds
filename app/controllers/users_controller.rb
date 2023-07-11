da29de586baf5132a6585ed887aca7eff0a8a6fc
thomas andrieu <than1544@colorado.edu>
2023-07-11 19:42:46 +0200

    all set up except sending to mutliple users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,12 +24,12 @@ class UsersController < ApplicationController
     redirect_to users_path, status: :see_other
   end
 
-  #def send_daily
-    #User.find_each do |user|
-     # NewsletterMailer.send_daily_newsletter(user.emailadress, "Le contenu de votre newsletter").deliver_now
-    #end
-    #redirect_to root_path, notice: 'Newsletter envoyée.'
-  #end
+  def send_daily
+    @user.find_each do |user|
+      UserMailer.send_daily_newsletter(user).deliver_now
+    end
+    redirect_to root_path, notice: 'Newsletter envoyée.'
+  end
 
 private
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
-  def send_daily_newsletter()
-    mail(to: 'thomasandrieu1997@gmail.com', subject: 'Votre fait historique quotidien')
+  def send_daily_newsletter(users)
+    @users = users
+    mail(to: users.emailadress, subject: 'Votre fait historique quotidien')
   end
 end
