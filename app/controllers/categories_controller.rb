c54595ca256f3e3d5d9d7e2402440e2baf78ff21
thomas andrieu <than1544@colorado.edu>
2023-11-04 14:44:50 +0100

    clean branch that's all

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,31 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.save
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path, status: :see_other
+  end
+
+end
+
+private
+
+def category_params
+  params.require(:category).permit(:name)
 end
